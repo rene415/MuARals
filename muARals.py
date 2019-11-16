5ea14040c497c0e3a3ded567e8f6ce4b208a1005
Kapilesh Kothavale <kapileshkothavale@gmail.com>
2019-11-16 11:37:37 -0800

    Update muARals.py

--- muARals.py
+++ muARals.py
@@ -8,7 +8,7 @@ import json
 
 #Collecting the page
 page = requests.get('https://freedomarchives.org/La_Lucha_Continua/Martin_Luther_King.html')
-print(page)
+#print(page)
 soup = BeautifulSoup(page.text, 'html.parser')
 
 
@@ -20,8 +20,9 @@ my_dict = {
     "person" : "Martin Luther King Jr",
     "message" : text
 }
-print(my_dict)
+#print(my_dict)
 
 d = json.dumps(my_dict)
-print(d)
+#print(d)
+sys.exit(d)
 
